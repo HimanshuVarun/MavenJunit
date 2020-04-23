b491baea43b68c57645efdd3e9b78bb6489c0f7d
HimanshuVarun <himanshuvarun9756@gmail.com>
2020-04-23 21:02:30 +0530

    first commit

---
+++ src/test/java/MavenJunit/MavenJunit/Hello4.java
@@ -0,0 +1,18 @@
+package MavenJunit.MavenJunit;
+import org.junit.Test;
+import org.testng.annotations.BeforeTest;
+public class Hello4 
+{
+	@BeforeTest
+	public void firstTest()
+	{
+		System.out.println("Hello innnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn junit ");
+	}
+	@Test
+	public void lastTest()
+	{
+		System.out.println("Hiii innnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn junit ");
+
+	}
+
+}
